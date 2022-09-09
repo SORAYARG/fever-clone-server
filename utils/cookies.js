47ec13e37be224197ab69677de8fc78ea8f60db6
Soraya Ruiz González <98762598+SORAYARG@users.noreply.github.com>
2022-09-09 18:09:46 +0200

    feat: add utils (#14)
    * feat: added auth services and middlewares
    
    * feat: auth-service

---
+++ utils/cookies.js
@@ -0,0 +1,17 @@
+const create = (res, token, extTime = 300000) => {
+    res.cookie("access_token", token, {
+      expires: new Date(Date.now() + extTime),
+      secure: false,
+      httpOnly: true,
+    });
+  };
+  
+  const clear = (res) => {
+    res.clearCookie("access_token");
+  };
+  
+  module.exports = {
+    create,
+    clear,
+  };
+  

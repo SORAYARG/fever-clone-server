2ce102237113a21f226c7fde6ca969a32c801761
sorayarg <sorayarg25@gmail.com>
2022-09-20 15:00:02 +0200

    feat: add service and queries

--- service/categories-users/index.js
+++ service/categories-users/index.js
@@ -2,7 +2,8 @@ const router = require("express").Router();
 const { authorizer } = require("../../middlewares");
 
 module.exports = (db) => {
-  router.post("/", authorizer, require("./insert")(db));
+  router.post("/insert", authorizer, require("./insert")(db));
+  router.get("/get-all", authorizer, require("./get-all")(db));
 
   return router;
 };
